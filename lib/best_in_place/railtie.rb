4b876241d771f133400b244884063bcd918b3f8e
Khairi Adnan <khairi@labs.my>
2022-06-29 23:59:55 +0800

    Update railtie.rb

--- lib/best_in_place/railtie.rb
+++ lib/best_in_place/railtie.rb
@@ -3,8 +3,8 @@ require 'action_view/base'
 
 module BestInPlace
   class Railtie < ::Rails::Railtie #:nodoc:
-    config.after_initialize do
-      BestInPlace::ViewHelpers = ActionView::Base.new({}, {}, "")
+    ActiveSupport.on_load(:action_controller_base) do
+      ActionController::Base.send(:helper, BestInPlace::ViewHelpers)
     end
   end
 end
