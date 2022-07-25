fbe5fe260a8ce5353b7578b89b026985515d475f
Anthony Richardson <26676+Osseta@users.noreply.github.com>
2022-07-25 12:24:47 +0930

    fix deprecation warning
    DEPRECATION WARNING: ActionView::Base instances should be constructed with a lookup context, assignments, and a controller.

--- lib/best_in_place/railtie.rb
+++ lib/best_in_place/railtie.rb
@@ -5,7 +5,7 @@ module BestInPlace
   class Railtie < ::Rails::Railtie #:nodoc:
     initializer 'best_in_place', after: :prepend_helpers_path do |app|
       ActiveSupport.on_load(:after_initialize) do
-        BestInPlace::ViewHelpers = ActionView::Base.new({}, {}, "")
+        BestInPlace::ViewHelpers = ActionView::Base.new(ActionView::Base.build_lookup_context(nil), {}, "")
       end
     end
   end
